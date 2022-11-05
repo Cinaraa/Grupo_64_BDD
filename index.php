0cd9db5457b2080741aef7c1f23ccf59eb126bd4
unknown <beatriz.goycoolea@uc.cl>
2022-11-04 21:03:34 -0300

    app 14

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
   <form align="center" action="consultas/consulta_3.php" method="post">
     Nombre productora:
     <input type="text" name="nombre_productora_elegida">
+    <br>
     <br/>
     <input type="submit" value="Buscar">
   </form>
