4ff260570a01641a759acd3bc25c0052017eb385
unknown <beatriz.goycoolea@uc.cl>
2022-11-04 21:33:52 -0300

    app 21

--- consultas/consulta_3.php
+++ consultas/consulta_3.php
@@ -13,8 +13,10 @@
      WHERE e.productora = p.nombre_productora
      AND r.nombre_recinto = e.recinto
      AND r.pais = p.pais
-     AND e.fecha_inicio >= ALL (SELECT eventos.fecha_inicio FROM eventos, productoras, recintos WHERE eventos.productora = productoras.nombre_productora AND recintos.nombre_recinto = eventos.recinto AND recintos.pais = productoras.pais AND productoras.id_productora = p.id_productora) AND p.nombre_productora LIKE '%$nombre_nuevo%' AND p.pais LIKE '%$pais_nuevo%'
-     COLLATE Latin1_general_CI_AI;";
+     AND e.fecha_inicio >= ALL (SELECT eventos.fecha_inicio FROM eventos, productoras, recintos WHERE eventos.productora = productoras.nombre_productora AND recintos.nombre_recinto = eventos.recinto AND recintos.pais = productoras.pais AND productoras.id_productora = p.id_productora)
+    AND UPPER(p.nombre_productora) LIKE '%$nombre_nuevo%' 
+    AND UPPER(p.pais) LIKE '%$pais_nuevo%'
+    COLLATE Latin1_general_CI_AI;";
 
 
 	$result = $db -> prepare($query);
