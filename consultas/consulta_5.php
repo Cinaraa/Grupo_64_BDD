01b72bb4ff55706cb600f5ee5fa4cc2b50a7d2cb
unknown <beatriz.goycoolea@uc.cl>
2022-11-06 18:43:16 -0300

    app 55

--- consultas/consulta_5.php
+++ consultas/consulta_5.php
@@ -8,11 +8,12 @@
   $nombre_nuevo = $_POST["nombre_evento_elegido"];
 
 
-  $query = "SELECT SUM(entradas.precio)
+  $query = "SELECT entradas.nombre_evento SUM(entradas.precio) AS suma_evento
     FROM entradas, acceso, eventos
     where entradas.id_entrada = acceso.id_entrada
     AND acceso.id_evento = eventos.id_evento
-    AND UPPER(eventos.nombre_evento) LIKE UPPER('%$nombre_nuevo%');";
+    AND UPPER(eventos.nombre_evento) LIKE UPPER('%$nombre_nuevo%')
+    GROUP BY entradas.nombre_evento;";
 
 
   $result = $db -> prepare($query);
@@ -22,11 +23,12 @@
 
   <table>
     <tr>
+      <th>Evento</th>
       <th>Ingresos totales recaudados</th>
     </tr>
   <?php
 	foreach ($entradas as $entrada) {
-        echo "<tr><td>$entrada[0]</td></tr>";
+        echo "<tr><td>$entrada[0]</td><td>$entrada[1]</td></tr>";
   }
   ?>
   </table>
