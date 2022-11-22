302d36360df15d3608a77dfb02383cc72f0ad4aa
Nader Hadweh <nader.hadweh@uc.cl>
2022-11-22 19:23:43 -0300

    consul

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <body>
   <div class="columns is-centered">
-    <a class="is-underlined has-text-info" href="queries/importar_usuarios.php">Importar Usuarios</a>
+    <a class="is-underlined has-text-info" href="consultas/importar_usuarios.php">Importar Usuarios</a>
   </div>
 </body>
 </html>
