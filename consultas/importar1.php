3ea4bd4b2f1e535c2a52bffe64abaefb73c52dfd
Nader Hadweh <nader.hadweh@uc.cl>
2022-11-22 20:59:54 -0300

    b

--- consultas/importar1.php
+++ consultas/importar1.php
@@ -13,10 +13,10 @@
 
     foreach ($artistas as $artista){
 
-        // Luego construimos las querys con nuestro procedimiento almacenado para ir agregando esas tuplas a nuestra bdd objetivo
-        // Hacemos una verificacion para ver si el pokemon es legendario porque ese parámetro no se comporta muy bien entre php y sql
-        // asi que lo agregamos manualmente al final (por eso los FALSE o TRUE)
-        
         $query = "SELECT proc1($artista[0]);";
+
+        $result = $db65 -> prepare($query);
+        $result -> execute();
+        $result -> fetchAll();
     }
 ?>
