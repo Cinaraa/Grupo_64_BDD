64a52d47c5fd12205f626c60df6f737e69f25367
Nader Hadweh <nader.hadweh@uc.cl>
2022-11-25 16:22:40 -0300

    1

--- index.php
+++ index.php
@@ -5,5 +5,16 @@
   <div class="columns is-centered">
     <a class="is-underlined has-text-info" href="consultas/importar_usuarios.php">Importar Usuarios</a>
   </div>
+
+  <h1 align="center">Buscador informacion de eventos </h1>
+  <p style="text-align:center;">Aquí podrás encontrar información sobre tus eventos favoritos.</p>
+
+
+  <h3 align="center"> Entregue los nombres y numeros de contacto de todas las productoras de eventos</h3>
+
+  <form align="center" action="consultas/consulta_1.php" method="post">
+    <br>
+    <input type="submit" value="Buscar">
+  </form>
 </body>
 </html>
