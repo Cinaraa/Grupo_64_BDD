9148c650dd09b0dc9f55ffce3ade315439ed2d8a
FRANCOBARDI <franco.bardi@uc.cl>
2022-11-25 17:33:23 -0300

    d

--- consultas/importar_usuarios.php
+++ consultas/importar_usuarios.php
@@ -17,7 +17,7 @@
 	foreach ($artistas as $artista) {
         
 		$insert= $db65-> prepare("INSERT INTO $db65.usuarios (nombre_usuario, contrasena, tipo) values(?, ?, 'artista');")
-		$insert -> execute(array($artista.nombre_artista,floor(random()*999999-100000+1)+100000))
+		$insert -> execute(array($artista.nombre_artista,floor(random()*999999-100000+1)+100000));
 	}
   ?>
 
