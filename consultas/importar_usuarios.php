573083693fe3140cf450d7b370ca5a409e6c830d
unknown <beatriz.goycoolea@uc.cl>
2022-11-28 12:35:44 -0300

    4

--- consultas/importar_usuarios.php
+++ consultas/importar_usuarios.php
@@ -10,6 +10,12 @@
 	$result = $db65 -> prepare($query);
 	$result -> execute();
 	$artistas = $result -> fetchAll();
+
+	$query_2 = "SELECT * FROM productoras;";
+
+	$result_2 = $db64 -> prepare($query_2);
+	$result_2 -> execute();
+	$productoras = $result_2 -> fetchAll();
   ?>
 
 	
@@ -19,7 +25,11 @@
 		$insert = $db65-> prepare("INSERT INTO usuarios (nombre_usuario, contrasena, tipo) values(?, ?, 'artista');");
 		$insert -> execute(array($artista[0],strval(random_int(100000, 999999))));
 	}
+	foreach ($productoras as $productora) {
+        
+		$insert_2 = $db65-> prepare("INSERT INTO usuarios (nombre_usuario, contrasena, tipo) values(?, ?, 'productora');");
+		$insert_2 -> execute(array($productora[0],strval(random_int(100000, 999999))));
+	}
   ?>
-
 
 <?php include('../templates/footer.html'); ?>
