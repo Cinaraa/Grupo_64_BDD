bf8feb42ec5bdedca02d77496f8104aff172b2af
FRANCOBARDI <franco.bardi@uc.cl>
2022-11-30 18:38:49 -0300

    c

--- consultas/rechazar_evento.php
+++ consultas/rechazar_evento.php
@@ -13,15 +13,41 @@
     $nombre_artista = str_replace('_', ' ', $nombre_art);
     $nombre_productora = str_replace('%20', ' ', $productora);
 
-    $query_rechazo = "UPDATE eventos SET estado = 'rechazado' WHERE lower(nombre_artista) LIKE 'abba';"; 
+    $query_rechazo = "UPDATE eventos SET estado = 'rechazado' WHERE lower(nombre_artista) LIKE lower($nombre_artista);"; 
     
     #AND lower(nombre_productora) LIKE lower($nombre_productora) AND lower(nombre_evento) LIKE lower($nombre_evento);";
 
     $result_rechazo = $db65 -> prepare($query_rechazo);
     $result_rechazo -> execute();
+    ?>
 
+    <body>
+    <?php
+    #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+        require("../config/conexion.php");
+
+        
+        $rechazados = $result_rechazo -> fetchAll();
+    ?>
+
+        <table>
+        <tr>
+        <th>Tour</th>
+
+        </tr>
+    <?php
+        
+            echo "<tr><td>lower($nombre_artista)</td></tr>";
+        
+    ?>
+        </table>
+
+    <?php
     #$msg = "$nombre_evento-$nombre_artista-$productora-$pais";
-    $msg = "$nombre_artista";
-    header("Location: ../index.php?msg=$msg");
+    $msg = "";
+    #header("Location: ../index.php?msg=$msg");
 
 ?>
+
+
+
