04d3298f26c8af76ddf6c0195d9dfca4bf9bc891
FRANCOBARDI <franco.bardi@uc.cl>
2022-11-30 18:23:45 -0300

    a

--- consultas/rechazar_evento.php
+++ consultas/rechazar_evento.php
@@ -13,8 +13,10 @@
     $nombre_artista = str_replace('_', ' ', $nombre_art);
     $nombre_productora = str_replace('%20', ' ', $productora);
 
-    $query_rechazo = "UPDATE eventos SET estado = 'rechazado' WHERE lower(nombre_artista) LIKE lower($nombre_artista) 
-    AND lower(nombre_productora) LIKE lower($nombre_productora) AND lower(nombre_evento) LIKE lower($nombre_evento);";
+    $query_rechazo = "UPDATE eventos SET estado = 'rechazado' WHERE lower(nombre_artista) LIKE lower($nombre_artista);"; 
+    
+    #AND lower(nombre_productora) LIKE lower($nombre_productora) AND lower(nombre_evento) LIKE lower($nombre_evento);";
+    
     $result_rechazo = $db65 -> prepare($query_rechazo);
     $result_rechazo -> execute();
 
