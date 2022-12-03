cb3bdbfbc13265e22eff7c2322a93deb2e086a84
unknown <beatriz.goycoolea@uc.cl>
2022-12-02 21:15:22 -0300

    24

--- index.php
+++ index.php
@@ -17,9 +17,23 @@
             <li><a herf='#'>Hospedajes</a></li>
           </ul>
         <?php } ?>
+        <?php if ($_SESSION['tipo'] == 'productora') { ?>
+          <ul>
+            <li><a herf='#'>Eventos programados</a></li>
+            <li><a herf='#'>Pendientes</a></li>
+            <li><a herf='#'>Aceptados</a></li>
+            <li><a herf='#'>rechazados</a></li>
+            <li><a herf='#'>Hospedajes</a></li>
+          </ul>
+        <?php } ?>
       <?php } ?>
       </img>
     </div>
+    <div class="content">
+      <?php if(!isset($_SESSION['nombre_usuario'])){?>
+        <h1>Iniciar Sesion:</h1>
+      <?php } ?>
+        <a class="is-underlined has-text-info" href="consultas/importar_usuarios.php">Importar  Usuarios</a>
   </div>
   <div class="columns is-centered">
     <a class="is-underlined has-text-info" href="consultas/importar_usuarios.php">Importar Usuarios</a>
